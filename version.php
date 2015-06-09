3a96264ce16742ad71e11acea22d3f32ec3e1259
Adam Franco <afranco@middlebury.edu>
2015-06-09 10:54:59 -0400

    Bumped version and added maturity, release properties.

--- version.php
+++ version.php
@@ -26,6 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015060400;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2015060900;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = 1.0.0;  
 $plugin->requires  = 2013111800;        // Requires this Moodle version.
 $plugin->component = 'auth_casattras';        // Full name of the plugin (used for diagnostics).
+$plugin->maturity = MATURITY_STABLE;
+
